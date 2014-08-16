76444eacc56c83a6dee6de30e9c1c899e29ac516
Daniel Dihardja <daniel.dihardja@socian.net>
2014-08-16 15:09:53 +0200

    Base class for the all view controllers

---
+++ Scripts/Controller/Controller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// the base controller 
+public class Controller : MonoBehaviour 
+{
+	// injected models that the controllers needs for their view components
+	protected GameModel gameModel;
+	protected PlayerStatusModel playerStatusModel;
+	protected InventoryVM inventoryVM;
+
+	// all controlers that inherit from this will have
+	// the models that are injected into its constructor
+	public void Init( GameModel model, PlayerStatusModel psModel, InventoryVM ivm )
+	{
+
+		gameModel = model;
+		playerStatusModel = psModel;
+		inventoryVM = ivm;
+
+		OnInitComplete();
+	}
+
+
+	protected virtual void OnInitComplete()
+	{
+		// this method will be overriden by the controllers
+		// that are inheriting this base controller class
+	}
+}
