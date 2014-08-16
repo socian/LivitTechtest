b972f99dcd157e36b4bf0052221a20669afdc719
Daniel Dihardja <daniel.dihardja@socian.net>
2014-08-16 15:12:00 +0200

    Toolbar controller class that initialzes and handles the event of the toolbar view component

---
+++ Scripts/Controller/ToolbarController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToolbarController : Controller 
+{
+	// ref to the toolbar view component
+	public ToolbarComponent toolbarComponent;
+
+	override protected void OnInitComplete()
+	{
+		// register the toolbar view component events
+		toolbarComponent.OnToolbarClick -= OnToolbarButtonClick;
+		toolbarComponent.OnToolbarClick += OnToolbarButtonClick;
+
+		// initialize the toolbar view component
+		toolbarComponent.Init();
+	}
+
+	void OnToolbarButtonClick(string command)
+	{
+		if(command == "save")
+		{
+			// tell the player status model to save its state
+			playerStatusModel.Save();
+		}
+		else if(command == "load") 
+		{
+			// tell the player status model to load its saved data
+			// the player status model will notify en event after
+			// the saved data is loaded. 
+			playerStatusModel.Load();
+		}
+	}
+}
+
