8c67396b9f62e150f7db3d28ef3516d50003fe33
Daniel Dihardja <daniel.dihardja@socian.net>
2014-08-16 15:11:19 +0200

    The inventory controller class that initializes and handles the events of the inventory view component

---
+++ Scripts/Controller/InventoryController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryController : Controller 
+{
+
+	public InventoryComponent inventoryComponent;
+
+	override protected void OnInitComplete()
+	{
+		// start the inventory view component
+		inventoryComponent.Init( inventoryVM );
+	}
+}
+
+
+
