996430cd41cd7fab3d89fe3761317037c38ea45d
Daniel Dihardja <daniel.dihardja@socian.net>
2014-08-16 15:16:19 +0200

    The view model of a level

---
+++ Scripts/Model/LevelVM.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelVM 
+{
+	
+	Level level;
+	
+	public LevelVM(Level l)
+	{
+		level = l;
+	}
+
+	public Level Level
+	{
+		get 
+		{
+			return level;
+		}
+	}
+
+	public StepVM GetStepVM(int index)
+	{
+		Step step = level.StepList[index];
+		return new StepVM(step);
+	}
+
+	public StepVM GetFirstStepVM()
+	{
+		Step step = level.StepList[0];
+		return new StepVM(step);
+	}
+
+	public StepVM GetStepVM(string id)
+	{
+		for(int i=0; i<level.StepList.Length; i++)
+		{
+			Step step = level.StepList[i];
+			if(step.Id == id) return new StepVM(step);
+		}
+		return null;
+	}
+}
+
+
+
