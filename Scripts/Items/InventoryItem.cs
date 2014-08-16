e2296aee8a20949e3a58333c082a1075e47eda1c
Daniel Dihardja <daniel.dihardja@socian.net>
2014-08-16 15:12:54 +0200

    inventory view item class. provides public variables for accessing the gui parts such as label and sprite

---
+++ Scripts/Items/InventoryItem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// utility item class for accessing the gui parts
+// such as the labels and the sprites
+public class InventoryItem : MonoBehaviour 
+{
+
+	public UISprite sprite;
+	public UILabel labelName;
+	public UILabel labelAmount;
+}
+
+
+
